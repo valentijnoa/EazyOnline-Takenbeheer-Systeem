4d68bc2300da7740103a39545907e20dc1c9b35a
valentijnoa <74962322+valentijnoa@users.noreply.github.com>
2024-05-13 16:04:49 +0200

    tekst

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import "./login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [error, setError] = useState(false);
+
+  const handeLogin = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handeLogin}>
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button type="submit">Login</button>
+        {error && <span>Incorrect email or password</span>}
+      </form>
+    </div>
+  );
 };
 
 export default Login;
